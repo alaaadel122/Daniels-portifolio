29f6b2eb61853899f9ca7636b013c5f6c70b5bb7
alaaadel122 <54372974+alaaadel122@users.noreply.github.com>
2025-05-12 20:51:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.nav-fixed');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+       
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
